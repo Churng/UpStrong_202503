47cdc67cb9c83b2f0a62c5f1aeda274c59b89e1e
Lynne.Lee <61216031+Churng@users.noreply.github.com>
2025-06-14 09:09:40 +0800

    新增：簽到頁面修改好

--- pages/ServiceCheckIn/style.js
+++ pages/ServiceCheckIn/style.js
@@ -18,8 +18,25 @@ $(document).ready(function () {
 	const urlSearchParams = new URLSearchParams(window.location.search);
 
 	const params = Object.fromEntries(urlSearchParams.entries());
-
+	const currentUserType = sessionStorage.getItem("userType");
 	console.log(params);
+	console.log(typeof currentUserType);
+
+	//設定返回按鈕
+	if (currentUserType === "1") {
+		$("#prevBtn")
+			.show()
+			.on("click", function () {
+				window.history.back();
+			});
+		$("#submit").hide();
+	} else if (currentUserType === "2") {
+		$("#prevBtn").hide();
+		$("#submit").show();
+	} else {
+		$("#prevBtn").hide();
+		$("#submit").hide();
+	}
 
 	//送出按鈕設定
 	$(".box02 .record-box .type-box span").on("click", function () {
@@ -31,12 +48,14 @@ $(document).ready(function () {
 		let formData = new FormData();
 
 		let session_id = sessionStorage.getItem("sessionId");
+		console.log(session_id);
 
 		let action = "getWorkOrderSignInDetailById";
 
 		let chsm = "upStrongWorkOrderApi"; // api文件相關
 
 		chsm = $.md5(session_id + action + chsm);
+		console.log(chsm);
 
 		let data = { workOrderId: params.workOrderID };
 
@@ -76,34 +95,38 @@ $(document).ready(function () {
 					$("#input-temp").val(item.BodyTemperature);
 
 					//進食
-					$("#SC-EatingLastScore").val(item.EatingLastScore);
+					$("#SC-EatingLastScore").text(item.EatingLastScore);
 					$("#SC-EatingCurrentScore").val(item.EatingCurrentScore);
 
 					//穿衣
-					$("#SC-DressingLastScore").val(item.DressingLastScore);
+					$("#SC-DressingLastScore").text(item.DressingLastScore);
 					$("#SC-DressingCurrentScore").val(item.DressingCurrentScore);
 
 					//穿褲
-					$("#SC-PantsDressingLastScore").val(item.PantsDressingLastScore);
+					$("#SC-PantsDressingLastScore").text(item.PantsDressingLastScore);
 					$("#SC-PantsDressingCurrentScore").val(item.PantsDressingCurrentScore);
 
 					//床邊坐起
-					$("#SC-BedsideSittingLastScore").val(item.BedsideSittingLastScore);
+					$("#SC-BedsideSittingLastScore").text(item.BedsideSittingLastScore);
 					$("#SC-BedsideSittingCurrentScore").val(item.BedsideSittingCurrentScore);
 
 					//站起
-					$("#SC-StandingLastScore").val(item.StandingLastScore);
+					$("#SC-StandingLastScore").text(item.StandingLastScore);
 					$("#SC-StandingCurrentScore").val(item.StandingCurrentScore);
 
 					//床椅移位
-					$("#SC-BedWheelchairTransferLastScore").val(item.BedWheelchairTransferLastScore);
+					$("#SC-BedWheelchairTransferLastScore").text(item.BedWheelchairTransferLastScore);
 					$("#SC-BedWheelchairTransferCurrentScore").val(item.BedWheelchairTransferCurrentScore);
+
+					//輪椅坐入
+					$("#SC-WheelchairEntryLastScore").text(item.WheelchairEntryLastScore);
+					$("#SC-WheelchairEntryCurrentScore").val(item.WheelchairEntryCurrentScore);
 
 					//輪椅選擇
 					$("#SC-UseManualWheelchairCurrentScore").val(item.UseManualWheelchairCurrentScore);
 					$("#SC-UsePowerWheelchairCurrentScore").val(item.UsePowerWheelchairCurrentScore);
-					$("#SC-UseManualWheelchairLastScore").val(item.UseManualWheelchairLastScore);
-					$("#SC-UsePowerWheelchairLastScore").val(item.UsePowerWheelchairLastScore);
+					$("#SC-UseManualWheelchairLastScore").text(item.UseManualWheelchairLastScore);
+					$("#SC-UsePowerWheelchairLastScore").text(item.UsePowerWheelchairLastScore);
 
 					if (item.UsePowerWheelchair === true) {
 						$("#SC-UsePowerWheelchair").prop("checked", true);
@@ -116,20 +139,33 @@ $(document).ready(function () {
 						$("#SC-UseManualWheelchair").prop("checked", false);
 					}
 					//步行
-					$("#SC-WalkingLastScore").val(item.WalkingLastScore);
+					$("#SC-WalkingLastScore").text(item.WalkingLastScore);
 					$("#SC-WalkingCurrentScore").val(item.WalkingCurrentScore);
 
 					//如廁
-					$("#SC-ToiletLastScore").val(item.ToiletLastScore);
-					$("#SC-ToiletCurrentScore").val(item.ToiletCurrentScore);
+					$("#SC-ToiletingLastScore").text(item.ToiletingLastScore);
+					$("#SC-ToiletingCurrentScore").val(item.ToiletingCurrentScore);
 
 					//沐浴
-					$("#SC-BathingLastScore").val(item.BathingLastScore);
+					$("#SC-BathingLastScore").text(item.BathingLastScore);
 					$("#SC-BathingCurrentScore").val(item.BathingCurrentScore);
 
 					//爬樓梯
-					$("#SC-StairClimbingLastScore").val(item.StairClimbingLastScore);
+					$("#SC-StairClimbingLastScore").text(item.StairClimbingLastScore);
 					$("#SC-StairClimbingCurrentScore").val(item.StairClimbingCurrentScore);
+
+					//月度評估確認是要true的話，則禁用所有輸入框？
+					if (item.MonthlyEvaluationRequired === false) {
+						const container = document.getElementById("monthly-checkin-container");
+						if (container) {
+							const inputs = container.querySelectorAll("input, textarea, select, button");
+							inputs.forEach((el) => {
+								el.disabled = true;
+							});
+
+							container.classList.add("disabled-container");
+						}
+					}
 				}
 			},
 
@@ -171,12 +207,13 @@ $(document).ready(function () {
 			BedWheelchairTransferCurrentScore: $("#SC-BedWheelchairTransferCurrentScore").val(),
 			UseManualWheelchairCurrentScore: $("#SC-UseManualWheelchairCurrentScore").val(),
 			UsePowerWheelchairCurrentScore: $("#SC-UsePowerWheelchairCurrentScore").val(),
+			WheelchairEntryCurrentScore: $("#SC-WheelchairEntryCurrentScore").val(),
 
 			UsePowerWheelchair: $("#SC-UsePowerWheelchair").prop("checked"),
 			UseManualWheelchair: $("#SC-UseManualWheelchair").prop("checked"),
 
 			WalkingCurrentScore: $("#SC-WalkingCurrentScore").val(),
-			ToiletCurrentScore: $("#SC-ToiletCurrentScore").val(),
+			ToiletingCurrentScore: $("#SC-ToiletingCurrentScore").val(),
 			BathingCurrentScore: $("#SC-BathingCurrentScore").val(),
 			StairClimbingCurrentScore: $("#SC-StairClimbingCurrentScore").val(),
 		};
@@ -203,75 +240,10 @@ $(document).ready(function () {
 			success: function (res) {
 				console.log(res);
 				handleResponse(res);
-				// if (res.returnCode == "1" && res.returnData) {
-				// 	let data = res.returnData;
-				// 	let item = data.workOrderSignInDetailData;
-
-				// 	// Box-1
-				// 	$("#input-hr-rest").val(item.PulseAtRest);
-				// 	$("#input-hr-exercise").val(item.PulseTrainingPercentMaxHr);
-				// 	$("#input-bp-rest").val(item.BpAtRest);
-				// 	$("#input-bp-exercise").val(item.BpDuringTraining);
-				// 	$("#input-spo2").val(item.OxygenSaturationSpo2);
-				// 	$("#input-rr").val(item.RespiratoryRate);
-				// 	$("#input-temp").val(item.BodyTemperature);
-
-				// 	//進食
-				// 	$("#SC-EatingLastScore").val(item.EatingLastScore);
-				// 	$("#SC-EatingCurrentScore").val(item.EatingCurrentScore);
-
-				// 	//穿衣
-				// 	$("#SC-DressingLastScore").val(item.DressingLastScore);
-				// 	$("#SC-DressingCurrentScore").val(item.DressingCurrentScore);
-
-				// 	//穿褲
-				// 	$("#SC-PantsDressingLastScore").val(item.PantsDressingLastScore);
-				// 	$("#SC-PantsDressingCurrentScore").val(item.PantsDressingCurrentScore);
-
-				// 	//床邊坐起
-				// 	$("#SC-BedsideSittingLastScore").val(item.BedsideSittingLastScore);
-				// 	$("#SC-BedsideSittingCurrentScore").val(item.BedsideSittingCurrentScore);
-
-				// 	//站起
-				// 	$("#SC-StandingLastScore").val(item.StandingLastScore);
-				// 	$("#SC-StandingCurrentScore").val(item.StandingCurrentScore);
-
-				// 	//床椅移位
-				// 	$("#SC-BedWheelchairTransferLastScore").val(item.BedWheelchairTransferLastScore);
-				// 	$("#SC-BedWheelchairTransferCurrentScore").val(item.BedWheelchairTransferCurrentScore);
-
-				// 	//輪椅選擇
-				// 	$("#SC-UseManualWheelchairCurrentScore").val(item.UseManualWheelchairCurrentScore);
-				// 	$("#SC-UsePowerWheelchairCurrentScore").val(item.UsePowerWheelchairCurrentScore);
-				// 	$("#SC-UseManualWheelchairLastScore").val(item.UseManualWheelchairLastScore);
-				// 	$("#SC-UsePowerWheelchairLastScore").val(item.UsePowerWheelchairLastScore);
-
-				// 	if (item.UsePowerWheelchair === true) {
-				// 		$("#SC-UsePowerWheelchair").prop("checked", true);
-				// 		$("#SC-UseManualWheelchair").prop("checked", false);
-				// 	} else if (item.UseManualWheelchair === true) {
-				// 		$("#SC-UsePowerWheelchair").prop("checked", false);
-				// 		$("#SC-UseManualWheelchair").prop("checked", true);
-				// 	} else {
-				// 		$("#SC-UsePowerWheelchair").prop("checked", false);
-				// 		$("#SC-UseManualWheelchair").prop("checked", false);
-				// 	}
-				// 	//步行
-				// 	$("#SC-WalkingLastScore").val(item.WalkingLastScore);
-				// 	$("#SC-WalkingCurrentScore").val(item.WalkingCurrentScore);
-
-				// 	//如廁
-				// 	$("#SC-ToiletLastScore").val(item.ToiletLastScore);
-				// 	$("#SC-ToiletCurrentScore").val(item.ToiletCurrentScore);
-
-				// 	//沐浴
-				// 	$("#SC-BathingLastScore").val(item.BathingLastScore);
-				// 	$("#SC-BathingCurrentScore").val(item.BathingCurrentScore);
-
-				// 	//爬樓梯
-				// 	$("#SC-StairClimbingLastScore").val(item.StairClimbingLastScore);
-				// 	$("#SC-StairClimbingCurrentScore").val(item.StairClimbingCurrentScore);
-				// }
+				if (res.returnCode == "1" && res.returnData) {
+					successResponse(res);
+					window.history.back();
+				}
 			},
 
 			error: function () {
